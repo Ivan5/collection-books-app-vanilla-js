39dccdce85f4b8e565af38698565d819322f5fe5
Ivan5 <converse_ivan@hotmail.com>
2020-05-21 13:45:11 -0500

    Make the class definition

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//definicion de las clases
+class Libro {
+  constructor(titulo, autor, isbn) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  static mostrarLibros() {}
+
+  static agregarLibroLista(libro) {}
+
+  static eliminarLibro() {}
+
+  static mostrarAlerta(mensaje) {}
+
+  static limpiarCampos() {}
+}
+
+class Datos {
+  static traerLibros() {}
+
+  static agregarLibro(libro) {}
+
+  static removerLibro(isbn) {}
+}
