dc58a038a958209ed46d36f6dee9c454c7497aff
Ivan5 <converse_ivan@hotmail.com>
2020-05-21 14:10:10 -0500

    Add book in localStorage

--- js/app.js
+++ js/app.js
@@ -14,15 +14,61 @@ class UI {
 
   static eliminarLibro() {}
 
-  static mostrarAlerta(mensaje) {}
+  static mostrarAlerta(mensaje, clase) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${clase}`;
+    div.appendChild(document.createTextNode(mensaje));
 
-  static limpiarCampos() {}
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#libro-form");
+    container.insertBefore(div, form);
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+
+  static limpiarCampos() {
+    document.querySelector("#titulo").value = "";
+    document.querySelector("#autor").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 class Datos {
-  static traerLibros() {}
+  static traerLibros() {
+    let libros;
+    if (localStorage.getItem("libros") === null) {
+      libros = [];
+    } else {
+      libros = JSON.parse(localStorage.getItem("libros"));
+    }
+    return libros;
+  }
 
-  static agregarLibro(libro) {}
+  static agregarLibro(libro) {
+    const libros = Datos.traerLibros();
+    libros.push(libro);
+    localStorage.setItem("libros", JSON.stringify(libros));
+  }
 
   static removerLibro(isbn) {}
 }
+
+//Controlar el evento submit
+document.querySelector("#libro-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //Obtener valores de los campos
+  const titulo = document.querySelector("#titulo").value;
+  const autor = document.querySelector("#autor").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  if (titulo === "" || autor === "" || isbn === "") {
+    UI.mostrarAlerta("Por favor ingresa todos los datos solicitados", "danger");
+  } else {
+    const libro = new Libro(titulo, autor, isbn);
+    Datos.agregarLibro(libro);
+    UI.limpiarCampos();
+  }
+});
